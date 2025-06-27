9768006e882b32709d19547eea0a01922f289a97
Vrushali Pandit <76472402+alivejojo@users.noreply.github.com>
2025-06-27 20:03:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,36 +1,5 @@
 # Free-form flows 
 
-This is the official `PyTorch` implementation for our papers:
-
-1. [Free-form flows: Make Any Architecture a Normalizing Flow](http://arxiv.org/abs/2310.16624) on full-dimensional normalizing flows:
-    ```bibtex
-    @inproceedings{draxler2024freeform,
-        title = {{Free-form flows: Make Any Architecture a Normalizing Flow}},
-        author = {Draxler, Felix and Sorrenson, Peter and Zimmermann, Lea and Rousselot, Armand and Köthe, Ullrich},
-        booktitle = {International Conference on Artificial Intelligence and Statistics},
-        year = {2024}
-    }
-    ```
-2. [Lifting Architectural Constraints of Injective Flows](http://arxiv.org/abs/2306.01843) on learning a manifold and the distribution on it jointly:
-    ```bibtex
-    @inproceedings{sorrenson2024lifting,
-        title = {{Lifting Architectural Constraints of Injective Flows}},
-        booktitle = {International {{Conference}} on {{Learning Representations}}},
-        author = {Sorrenson, Peter and Draxler, Felix and Rousselot, Armand and Hummerich, Sander and Zimmermann, Lea and Köthe, Ullrich},
-        year = {2024}
-    }
-    ```
-3. [Learning Distributions on Manifolds with Free-form Flows](https://arxiv.org/abs/2312.09852) on learning distributions on a known manifold:
-    ```bibtex
-    @article{sorrenson2023learning,
-        title = {Learning Distributions on Manifolds with Free-form Flows},
-        author = {Sorrenson, Peter and Draxler, Felix and Rousselot, Armand and Hummerich, Sander and Köthe, Ullrich},
-        journal = {arXiv preprint arXiv:2312.09852},
-        year = {2023}
-    }
-    ```
-
-
 ## Installation
 
 To run our experiments, install the dependencies first:
@@ -54,57 +23,26 @@ Then you can import the package via
 import fff
 ```
 
+## Vrushali's Experiments: 
 
-## Basic usage
+### Experiment 1 : UNet used for reconstruction and density learning of MNIST
+A lightweight U-Net was used as both encoder and decoder within the FFF framework to train on the MNIST dataset. This tests the flexibility of FFF with non-standard architectures and serves as a visual demonstration of one-step sampling and reconstruction.
 
-### Train your own Free-Form Flow 
+Run the "train_UNet.py" file. 
 
-See [toy-example.ipynb](toy-example.ipynb) for an example how to learn a model for toy data.
+**The only thing here that is borrowed from the original code is the surrogate_loss calculation.**
 
-### Tutorials
+### Experiment 2 : Gradient Curve Analysis
+We reproduced the cubic function toy example from Section 4.3 of the paper to empirically validate the claim that both the surrogate loss \( \mathcal{L}_g \) and the inverse loss \( \mathcal{L}_{f^{-1}} \) share the same critical points. We plot and compare gradients for both objectives across function parameterizations.
 
-Check out our tutorial on manifold free-form flows [here](https://drive.google.com/file/d/1z5qjozcVUcAr_X7UrrXmbol2ETMu_q86/view?usp=sharing)
+Look at the "gradient_plotting.ipynb". 
+To run your own functions, you will have to hand-calculate the Lf-1 and Lg and then update the functions. 
 
-### Reproduce our experiments
+**Nothing is borrowed from the original code**
 
-All training configurations from our papers can be found in the `configs/(fff|fif)` directories.
+### Everything apart from the following are the same as the original code:
+1. README.md
+2. train_UNet.py
+3. gradient_plotting.ipynb
 
-Our training framework is built on [lightning-trainable](https://github.com/LarsKue/lightning-trainable), a configuration wrapper around [PyTorch Lightning](https://lightning.ai/pytorch-lightning). There is no `main.py`, but you can train all our models via the `lightning_trainable.launcher.fit` module.
-For example, to train the Boltzmann generator on DW4:
-```bash
-python -m lightning_trainable.launcher.fit configs/fff/dw4.yaml --name '{data_set[name]}'
-```
-
-This will create a new directory `lightning_logs/dw4/`. You can monitor the run via `tensorboard`:
-```bash
-tensorboard --logdir lightning_logs
-```
-
-When training has finished, you can import the model via
-```python
-import fff
-
-model = fff.FreeFormFlow.load_from_checkpoint(
-    'lightning_logs/dw4/version_0/checkpoints/last.ckpt'
-)
-```
-
-If you want to overwrite the default parameters, you can add `key=value`-pairs after the config file:
-```bash
-python -m lightning_trainable.launcher.fit configs/fff/dw4.yaml batch_size=128 loss_weights.noisy_reconstruction=20 --name '{data_set[name]}'
-```
-
-#### Known issues
-
-Training with $E(n)$-GNNs is sometimes unstable. This is usually caught with an assertion in a later step and training is stopped.
-In almost all cases, training can be stably resumed from the last epoch checkpoint by passing the `--continue-from [CHECKPOINT]` flag to the training, such as:
-```bash
-python -m lightning_trainable.launcher.fit configs/fff/dw4.yaml --name '{data_set[name]}' --continue-from lightning_logs/dw4/version_0/checkpoints/last.ckpt
-```
-This reloads the entire training state (model state, optim state, epoch, etc.) from the checkpoint and continues training from there.
-
-
-### Setup your own training
-
-Start with the config file in `configs/(fff|fif)` that fits your needs best and modify it.
-For custom data sets, add the data set to `fff.data`.
+### I ran the UNet experiment over multiple ipynb files and google colab sessions, hence there are no logged plots here. But all the plots I used in my presentation are reproducible. 
---
+++ train_UNet.py
@@ -0,0 +1,304 @@
+import fff.loss as loss
+import fff.other_losses.exact_nll
+import torch
+import torchvision as tv
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+import torch.nn.utils as U
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+trainTransform  = tv.transforms.Compose([tv.transforms.ToTensor(), tv.transforms.Normalize((0.1307,), (0.3081,))])
+trainset = tv.datasets.MNIST(root='./data',  train=True,download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=2, pin_memory=True)
+testset = tv.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2, pin_memory=True)
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.Conv2d(1, 6, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(True),
+
+            nn.Conv2d(1, 6, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.Conv2d(1, 6, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(True),
+            nn.MaxPool2d((2, 2))
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(16,6,kernel_size=5),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(6,1,kernel_size=5),
+            nn.ReLU(True),
+            nn.Sigmoid())
+
+    def forward(self,x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+# Set device to GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
+
+# ---------- basic 3×3 conv block ----------
+class ConvBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, 3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+# ---------- encoder step ----------
+class EncoderBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv = ConvBlock(in_c, out_c)
+        self.pool = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        s = self.conv(x)
+        p = self.pool(s)
+        return s, p
+
+# ---------- decoder step ----------
+class DecoderBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.up   = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2)
+        self.conv = ConvBlock(in_c=out_c*2, out_c=out_c)
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        x = torch.cat([x, skip], dim=1)
+        return self.conv(x)
+
+# ---------- U-Net 28×28 grayscale ----------
+class ReconUNet28_Lite(nn.Module):
+    def __init__(self, in_ch=1):
+        super().__init__()
+        # ---- Encoder ----
+        self.e1 = EncoderBlock(in_ch, 16)    # 28×28 → 14×14
+        self.e2 = EncoderBlock(16, 32)       # 14×14 → 7×7
+
+        # ---- Bottleneck ----
+        self.bottleneck = ConvBlock(32, 64)
+
+        # ---- Decoder ----
+        self.d1 = DecoderBlock(64, 32)       # 7×7 → 14×14
+        self.d2 = DecoderBlock(32, 16)       # 14×14 → 28×28
+
+        # ---- Output layer ----
+        self.out_conv = nn.Conv2d(16, in_ch, kernel_size=1)
+
+    def forward(self, x):
+        s1, p1 = self.e1(x)
+        s2, p2 = self.e2(p1)
+        b      = self.bottleneck(p2)
+        d1     = self.d1(b, s2)
+        d2     = self.d2(d1, s1)
+        return self.out_conv(d2)
+
+
+class LatentDistribution(nn.Module):
+    def __init__(self,latent_dim, device):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.device = device
+
+    def get_distribution(self):
+        return torch.distributions.Independent(
+            torch.distributions.Normal(
+                loc=torch.zeros(latent_dim, device=self.device),
+                scale=torch.ones(latent_dim, device=self.device)
+            ),
+            1
+        )
+
+    def sample(self, sample_shape=torch.Size()):
+        return self.get_distribution().sample(sample_shape)
+
+    def log_prob(self, z):
+      if z.dim() != 2 or z.size(1) != 784:        # not yet flat to (B, 784)
+        z = z.view(z.size(0), -1)
+      return self.get_distribution().log_prob(z)
+
+
+# wrapping flow model
+class UNetFFF(nn.Module):
+    def __init__(self, latent_dim, device = 'cpu'):
+        # constructor
+        super().__init__()
+        self.encoder = ReconUNet28_Lite(in_ch=1).to(device)
+        self.decoder = ReconUNet28_Lite(in_ch=1).to(device)
+        self.latent = LatentDistribution(latent_dim, device)
+
+    def encode(self, x):
+        z_img = self.encoder(x) # this is after Unet_1
+        return z_img.view(z_img.size(0), -1) # returns a flattened [Batchsize, 784]
+
+    def decode(self, z): # this is going into Unet_2
+        z_img = z.view(z.size(0), 1, 28, 28) # reshaped to the img tensor
+        return self.decoder(z_img) # recon x
+
+    def log_prob_z(self, z):
+        # return self.latent.get_distribution().log_prob(z) # what shape should z be here to be able to calculate this?
+        return self.latent.log_prob(z)
+
+    def sample(self, num_samples):
+        # decode imgs after drawing samples from latent
+        z = self.latent.sample(torch.Size([num_samples]))  # [N,784]
+        return self.decode(z)
+
+
+latent_dim = 28*28
+model  = UNetFFF(latent_dim, device)
+
+
+n_epochs      = 15
+learning_rate = 1e-4
+k_hutch       = 4          # Hutchinson samples
+ema_tau       = 0.999
+latent_dim = 28*28
+
+# Store results across betas
+beta_train_loss = []
+beta_recon_loss = []
+beta_nll_loss   = []
+loss_history    = []
+# beta_surr_loss = []
+# beta_logprob_loss = []
+
+for beta in [0.01, 1, 10, 100]:
+    print("beta =", beta)
+
+    model = UNetFFF(latent_dim, device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = StepLR(optimizer, step_size=3, gamma=0.5)
+    model.sur_base = torch.zeros(1, device=device)
+
+    train_epoch_loss = []
+    recon_epoch_loss = []
+    nll_epoch_loss   = []
+    # surrs_loss = []
+    # log_probs_loss = []
+
+    for epoch in range(n_epochs):
+        print("epoch =", epoch)
+        model.train()
+        running_loss = running_recon = running_nll = 0.0
+
+        for x, _ in dataloader:
+            x = x.to(device)
+            optimizer.zero_grad()
+
+            # 1. Volume surrogate
+
+            ############################## from FFF package #############################
+            sur_out = fff.loss.volume_change_surrogate(
+                x, model.encoder, model.decoder,
+                hutchinson_samples=k_hutch
+            )
+            ############################## from FFF package #############################
+
+            d  = 784          # 784 for MNIST
+
+            sur_scaled = sur_out.surrogate / d
+            model.sur_base = ema_tau * model.sur_base + (1 - ema_tau) * sur_scaled.mean()
+            sur_centered  = sur_scaled - model.sur_base.detach()
+
+            # 2. Reconstruction
+            x1 = sur_out.x1
+            lrecon = (x - x1).pow(2).flatten(1).sum(-1)/784
+
+            # 3. Latent log prob
+            z_flat   = sur_out.z.flatten(1)
+            log_prob = model.latent.get_distribution().log_prob(z_flat)
+            log_prob = log_prob / 784
+            # surrs_loss.append(- sur_centered.mean().item())  # volume surrogate
+            # log_probs_loss.append(- log_prob.mean().item())  # log-prob
+
+            # 4. Final losses
+            lnll = -log_prob - sur_centered
+            loss = (beta * lrecon + lnll).mean()
+
+            loss.backward()
+            if epoch == 0:
+                for n, p in model.encoder.named_parameters():
+                    if p.grad is None:
+                        print(f"[WARNING] no grad for {n}")
+            U.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+
+            # Accumulate batch losses
+            running_loss  += loss.item()
+            running_recon += lrecon.mean().item()
+            running_nll   += lnll.mean().item()
+
+        scheduler.step()
+        loss_history.append({"epoch": epoch, "lr": scheduler.get_last_lr()[0]})
+
+        # Average over batches
+        avg_loss  = running_loss / len(dataloader)
+        avg_recon = running_recon / len(dataloader)
+        avg_nll   = running_nll / len(dataloader)
+
+        train_epoch_loss.append(avg_loss)
+        recon_epoch_loss.append(avg_recon)
+        nll_epoch_loss.append(avg_nll)
+
+        print(f"[Epoch {epoch}] total {avg_loss:.3f}  recon {avg_recon:.3f}  nll {avg_nll:.3f}")
+
+    beta_train_loss.append(train_epoch_loss)
+    beta_recon_loss.append(recon_epoch_loss)
+    beta_nll_loss.append(nll_epoch_loss)
+
+epochs = range(len(train_epoch_loss))
+
+plt.figure(figsize=(10, 6))
+plt.plot(epochs, train_epoch_loss, label='Total Loss')
+plt.plot(epochs, recon_epoch_loss, label='Reconstruction Loss')
+plt.plot(epochs, nll_epoch_loss, label='NLL Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss Curves over Epochs')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("loss_curves.png", dpi=300)
+plt.close()
+
+# Plot and save learning rate schedule
+epochs = [entry["epoch"] for entry in loss_history]
+lr_vals = [entry["lr"] for entry in loss_history]
+
+plt.figure(figsize=(7, 4))
+plt.plot(epochs, lr_vals, marker='o', linestyle='-')
+plt.xlabel('Epoch')
+plt.ylabel('Learning Rate')
+plt.title('Learning Rate Schedule over Epochs')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("lr_schedule.png", dpi=300)
+plt.close()
